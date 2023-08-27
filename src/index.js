2f6a78a82af02cfe497a21bf3817d64032e8db1f
vermaAtul1520 <atulverma1520@gmail.com>
2023-08-27 11:18:44 +0530

    setup done of websocket with socket.io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const path=require('path')
+const http=require('http')
 const express=require('express');
+const socketio=require('socket.io')
+
 const app=express();
+const server=http.createServer(app)
+const io=socketio(server);
 
 const port=process.env.port || 3000
 const publicDirectory=path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectory))
 
-app.listen(port, () => {
+io.on('connection',()=>{
+    console.log("New websocket connection!")
+})
+server.listen(port, () => {
     console.log(`Server is on port ${port}`)
 })
